c514a3e6f1fc18774a60c09ef4c6afd88fc7cf91
Vitor Trimer <vitortrimer@gmail.com>
2024-07-14 16:35:34 -0300

    Refactor world struct to accomplish player

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,18 @@ use crossterm::{
 struct World {
     maxc: u16,
     maxl: u16,
+    map: Vec<(u16, u16)>,
+    player: Player
+}
+
+struct Player {
     pc: u16,
     pl: u16,
-    map: Vec<(u16, u16)>,
+    dead: bool,
 }
 
 fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
     sc.queue(Clear(crossterm::terminal::ClearType::All))?;
-    sc.queue(MoveTo(world.pc, world.pl))?;
-    sc.queue(Print("P"))?;
-    sc.flush()?;
 
     for l in 0..world.map.len() {
         sc.queue(MoveTo(0, l as u16))?;
@@ -32,6 +34,10 @@ fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
         sc.queue(MoveTo(world.map[l].1, l as u16))?;
         sc.queue(Print("'".repeat((world.maxc - world.map[l].1).into())))?;
     }
+    sc.flush()?;
+    
+    sc.queue(MoveTo(world.player.pc, world.player.pl))?;
+    sc.queue(Print("P"))?;
     sc.flush()?;
 
     Ok(())
@@ -47,30 +53,32 @@ pub fn main() -> std::io::Result<()> {
     let mut world = World {
         maxc,
         maxl,
-        pc: maxc / 2,
-        pl: maxl - 1,
-        map: vec![(maxc/2-5, maxc/2+5); maxl as usize]
+        map: vec![(maxc/2-5, maxc/2+5); maxl as usize],
+        player: Player { pc: maxc/2, pl: maxl -1, dead: false },
     };
 
-    loop {
+   while !world.player.dead {
         if poll(Duration::from_millis(10))? {
             let key = read().unwrap();
+            while poll(Duration::from_millis(0)).unwrap() {
+                let _ = read();
+            }
             match key {
                 Event::Key(event) => match event.code {
                     KeyCode::Char('q') => {
                         break;
                     }
                     KeyCode::Char('w') => {
-                        if world.pl > 1 { world.pl -= 1; }
+                        if world.player.pl > 1 { world.player.pl -= 1; }
                     }
                     KeyCode::Char('s') => {
-                        if world.pl < maxl { world.pl += 1; }
+                        if world.player.pl < maxl { world.player.pl += 1; }
                     }
                     KeyCode::Char('a') => {
-                        if world.pc > 1 { world.pc -= 1; }
+                        if world.player.pc > 1 { world.player.pc -= 1; }
                     }
                     KeyCode::Char('d') => {
-                        if world.pc < maxc { world.pc += 1; }
+                        if world.player.pc < maxc { world.player.pc += 1; }
                     }
                     _ => {}
                 },
