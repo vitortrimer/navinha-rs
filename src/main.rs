30dfcbb7e29aa835dceb5cce9a08fc66a5c9b7d4
Vitor Trimer <vitortrimer@gmail.com>
2024-07-14 14:27:35 -0300

    Add borders to the game

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,30 @@ use crossterm::{
     event::{self, poll, read, Event, KeyCode, KeyEvent},
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor},
-    terminal::{disable_raw_mode, enable_raw_mode, size},
+    terminal::{disable_raw_mode, enable_raw_mode, size, Clear},
     ExecutableCommand, QueueableCommand,
 };
 
 struct World {
+    maxc: u16,
+    maxl: u16,
     pc: u16,
     pl: u16,
+    map: Vec<(u16, u16)>,
 }
 
 fn draw(mut sc: &Stdout, world: &World) -> std::io::Result<()> {
+    sc.queue(Clear(crossterm::terminal::ClearType::All))?;
     sc.queue(MoveTo(world.pc, world.pl))?;
     sc.queue(Print("P"))?;
+    sc.flush()?;
+
+    for l in 0..world.map.len() {
+        sc.queue(MoveTo(0, l as u16))?;
+        sc.queue(Print("'".repeat(world.map[l].0.into())))?;
+        sc.queue(MoveTo(world.map[l].1, l as u16))?;
+        sc.queue(Print("'".repeat((world.maxc - world.map[l].1).into())))?;
+    }
     sc.flush()?;
 
     Ok(())
@@ -29,12 +41,15 @@ pub fn main() -> std::io::Result<()> {
     let mut sc = stdout();
     let (maxc, maxl) = size().unwrap();
 
-    sc.execute(Hide);
-    enable_raw_mode();
+    let _ = sc.execute(Hide);
+    let _ = enable_raw_mode();
 
     let mut world = World {
+        maxc,
+        maxl,
         pc: maxc / 2,
         pl: maxl - 1,
+        map: vec![(maxc/2-5, maxc/2+5); maxl as usize]
     };
 
     loop {
@@ -45,6 +60,18 @@ pub fn main() -> std::io::Result<()> {
                     KeyCode::Char('q') => {
                         break;
                     }
+                    KeyCode::Char('w') => {
+                        if world.pl > 1 { world.pl -= 1; }
+                    }
+                    KeyCode::Char('s') => {
+                        if world.pl < maxl { world.pl += 1; }
+                    }
+                    KeyCode::Char('a') => {
+                        if world.pc > 1 { world.pc -= 1; }
+                    }
+                    KeyCode::Char('d') => {
+                        if world.pc < maxc { world.pc += 1; }
+                    }
                     _ => {}
                 },
                 _ => {}
@@ -52,10 +79,10 @@ pub fn main() -> std::io::Result<()> {
         } else {
         }
 
-        draw(&sc, &world);
+        let _ = draw(&sc, &world);
     }
 
-    sc.execute(Show);
-    disable_raw_mode();
+    let _ = sc.execute(Show);
+    let _ = disable_raw_mode();
     Ok(())
 }
